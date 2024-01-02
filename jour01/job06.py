534e0edb6957170eee09ccd92a694f4142c43351
jean <jean@gmail.com>
2024-01-02 15:36:46 +0100

    job06 added

--- jour01/job06.py
+++ jour01/job06.py
@@ -2,10 +2,15 @@ class Animal:
     def __init__(self, age=0, prenom=""):
         self.age = age
         self.prenom = prenom
-
     def vieillir(self):
         self.age += 1
+    def nommer(self, prenom ="Luna"):
+        self.prenom = prenom
+        
 instance = Animal()
-print(f"L'âge de l'animal est {instance.age} ans")
+print(f"L'age de l'animal est {instance.age} ans")
 instance.vieillir()
-print(f"L'âge de l'animal est {instance.age} ans")
+print(f"L'age de l'animal est {instance.age} ans")
+instance.nommer()
+print(f"L'animal se nomme {instance.prenom}")
+
